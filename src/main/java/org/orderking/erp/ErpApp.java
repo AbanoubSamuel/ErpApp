5f5ce835b7fcc3cc093b72c62f43bbd13bee3ddb
Abanoub Samuel <abanoub.samuel@hotmail.com>
2024-01-10 20:02:05 +0200

    init: created initial files.

---
+++ src/main/java/org/orderking/erp/ErpApp.java
@@ -0,0 +1,14 @@
+package org.orderking.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpApp {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ErpApp.class, args);
+    }
+
+}
---
+++ src/test/java/org/orderking/erp/ErpAppTests.java
@@ -0,0 +1,14 @@
+package org.orderking.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpAppTests {
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
